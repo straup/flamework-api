dad88d1550ba251378d882c0935bf6ba980e2a00
straup <straup@gmail.com>
2012-01-22 22:11:40 +0100

    add hooks to except/test crumbs at the API layer

--- www/include/lib_api_auth.php
+++ www/include/lib_api_auth.php
@@ -11,7 +11,9 @@
 
 	#################################################################
 
-	function api_auth_has_auth(){
+	function api_auth_has_auth(&$method){
+
+		return ($GLOBALS['cfg']['user']['id']) ? 1 : 0;
 
 		# please write me...
 
@@ -19,4 +21,34 @@
 	}
 
 	#################################################################
+
+	function api_auth_ensure_crumb(&$method, $ttl=0){
+
+		if (! api_auth_has_valid_crumb($method, $ttl)){
+			api_output_error(999, "Missing or invalid crumb");
+		}
+	}
+
+	#################################################################
+
+	function api_auth_has_valid_crumb(&$method, $ttl=0){
+
+		$crumb = request_str("crumb");
+
+		if (! $crumb){
+			return 0;
+		}
+
+		$name = $method['name'];
+		$ttl = (isset($method['crumb_ttl'])) ? $method['crumb_ttl'] : 0;
+
+		if (! crumb_check("api", $ttl, $name)){
+			return 0;
+		}
+
+		return 1;
+	}
+
+	#################################################################
+
 ?>
