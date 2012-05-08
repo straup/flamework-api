93839214dbbac11dc8d1985aad7b69ac46ff956c
Aaron S Cope <straup@jionor.pair.com>
2012-05-08 15:42:23 -0400

    cross-platform WHOAMI... grnnnrnngnnng

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-WHOAMI=`readlink -f $0`
+if test $OSTYPE = "FreeBSD"
+then
+    WHOAMI=`realpath $0`
+elif test $OSTYPE = "darwin"
+then
+    WHOAMI=`python -c 'import os, sys; print os.path.realpath(sys.argv[1])' $0`
+else
+    WHOAMI=`readlink -f $0`    
+fi
+
 WHEREAMI=`dirname $WHOAMI`
 API=`dirname $WHEREAMI`
 
