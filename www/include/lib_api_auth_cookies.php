ee8195df03aa2ec22e317a105eac78cbd4518a05
straup <straup@gmail.com>
2013-03-27 18:32:26 -0400

    clean up errant instances of okay/not_okay

--- www/include/lib_api_auth_cookies.php
+++ www/include/lib_api_auth_cookies.php
@@ -7,13 +7,13 @@
 		$ok = ($GLOBALS['cfg']['user']['id']) ? 1 : 0;
 
 		if (! $ok){
-			return not_okay("Invalid user", 400);
+			return array('ok' => 0, 'error' => 'Invalid user', 'error_code' => 400);
 		}
 
 		if (isset($method['requires_perms'])){
 
 			if ($method['requires_perms'] != 0){
-				return not_okay("Insufficient permissions", 403);
+				return array('ok' => 0, 'error' => 'Insufficient permissions', 'error_code' => 403);
 			}
 		}
 
